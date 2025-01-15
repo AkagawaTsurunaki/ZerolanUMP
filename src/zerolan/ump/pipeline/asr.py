6ad2b0cff27ba75cee2e78a5072e2e3609634d3c
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2025-01-15 11:24:59 +0800

    feat: ASR 管线

---
+++ src/zerolan/ump/pipeline/asr.py
@@ -0,0 +1,72 @@
+from http import HTTPStatus
+from typing import Literal
+from urllib.parse import urljoin
+
+import requests
+from loguru import logger
+from pydantic import BaseModel
+from zerolan.data.pipeline.asr import ASRQuery, ASRPrediction, ASRStreamQuery
+
+from src.zerolan.ump.abs_pipeline import AbstractPipeline
+from zerolan.ump.common.decorator import pipeline_resolve
+
+
+class ASRPipelineConfig(BaseModel):
+    enable: bool = True
+    server_url: str = "http://127.0.0.1:11001"
+    sample_rate: int = 16000
+    channels: int = 1
+    format: Literal["float32"] = "float32"
+
+
+class ASRPipeline(AbstractPipeline):
+
+    def __init__(self, config: ASRPipelineConfig):
+        super().__init__(config)
+        self.predict_url = urljoin(config.server_url, "/asr/predict")
+        self.stream_predict_url = urljoin(config.server_url, '/asr/stream-predict')
+        self.state_url = urljoin(config.server_url, '/asr/state')
+        self.check_urls()
+
+    @pipeline_resolve()
+    def predict(self, query: ASRQuery) -> ASRPrediction | None:
+        assert isinstance(query, ASRQuery)
+        try:
+            files, data = self.parse_query(query)
+            response = requests.post(url=self.predict_url, files=files, data=data)
+
+            if response.status_code == HTTPStatus.OK:
+                prediction = self.parse_prediction(response.content)
+                return prediction
+
+        except Exception as e:
+            logger.exception(e)
+            return None
+
+    @pipeline_resolve()
+    def stream_predict(self, query: ASRStreamQuery):
+        files, data = self.parse_query(query)
+        response = requests.get(url=self.stream_predict_url, files=files, data=data)
+
+        if response.status_code == HTTPStatus.OK:
+            return self.parse_prediction(response.content)
+        else:
+            response.raise_for_status()
+
+    def parse_query(self, query: ASRQuery | ASRStreamQuery) -> tuple:
+        if isinstance(query, ASRQuery):
+            files = {"audio": open(query.audio_path, 'rb')}
+            data = {"json": query.model_dump_json()}
+
+            return files, data
+        elif isinstance(query, ASRStreamQuery):
+            files = {"audio": query.audio_data}
+            query.audio_data = ""
+            data = {"json": query.model_dump_json()}
+
+            return files, data
+        else:
+            raise ValueError("Can not convert query.")
+
+    def parse_prediction(self, json_val: str) -> ASRPrediction:
+        return ASRPrediction.model_validate_json(json_val)
